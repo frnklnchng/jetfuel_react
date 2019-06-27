8abbb747ea2f81708e58faa4ddb67f9718944f1b
Franklin Chang <frnklnchng@gmail.com>
2019-06-26 20:08:08 -0700

    Complete most functionality

--- app/src/components/campaign_media_item.jsx
+++ app/src/components/campaign_media_item.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
 
 class CampaignMediaItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.copyToClipboard = this.copyToClipboard.bind(this);
+  }
+
+  copyToClipboard(str) {
+    const el = document.createElement('textarea');
+
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+
+    let selected = false;
+
+    if (document.getSelection().rangeCount > 0) {
+      selected = document.getSelection().getRangeAt(0);
+    }
+
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    if (selected) {
+      document.getSelection().removeAllRanges();
+      document.getSelection().addRange(selected);
+    }
+  };
+
   render() {
     const {cover_photo_url, media_type, tracking_link, download_url} = this.props.media;
     const resize = "https://res.cloudinary.com/zzyzx/image/fetch/w_200,h_400,c_fill,g_face,r_10,f_auto/";
@@ -18,40 +49,15 @@ class CampaignMediaItem extends React.Component {
       );
     };
 
-    const copyToClipboard = (str) => {
-      const el = document.createElement('textarea');
-
-      el.value = str;
-      el.setAttribute('readonly', '');
-      el.style.position = 'absolute';
-      el.style.left = '-9999px';
-      document.body.appendChild(el);
-
-      let selected = false;
-
-      if (document.getSelection().rangeCount > 0) {
-        selected = document.getSelection().getRangeAt(0);
-      }
-
-      el.select();
-      document.execCommand('copy');
-      document.body.removeChild(el);
-      
-      if (selected) {
-        document.getSelection().removeAllRanges();
-        document.getSelection().addRange(selected);
-      }
-    };
-
     return (
       <li>
         <div>
           {coverMedia()}
-          <a href={tracking_link}>
-            <img className="" src={null} alt="Plug" />
-          </a>
+          <button onClick={() => {this.copyToClipboard(tracking_link)}}>
+            <img className="" src={null} alt="Copy"/>
+          </button>
           <a href={download_url}>
-            <img className="" src={null} alt="Plug" />
+            <img className="" src={null} alt="Download"/>
           </a>
         </div>
       </li>
